381f9421ee938cb3892d3d11cd80c618cf97cf88
Cyrus Yip <cyrusyip@cyrusyip.com>
2025-01-18 22:19:35 -0500

    header

--- app.vue
+++ app.vue
@@ -1,4 +1,12 @@
 <template>
+  <header class="p-5">
+    <a
+      href="/"
+      class="text-white flex flex-row absolute justify-center items-center gap-3"
+      ><img src="public/logo.png" alt="" class="w-10" />
+      <span>lectureboxd</span></a
+    >
+  </header>
   <div class="text-white min-h-screen flex items-center justify-center text-lg">
     <NuxtPage />
   </div>
