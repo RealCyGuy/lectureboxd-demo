9d61580189789da73a95683673f9bbc2e007cc13
isabel-lb <isabellaingg@gmail.com>
2025-01-18 21:28:46 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center flex-col gap-5">
     <h1 class="text-xl">
-      Welcome to lectureboxd, the ... for McGill students!
+      Welcome to LectureBoxd... the ultimate network for McGill students!
     </h1>
     <NuxtLink
       class="bg-gray-800 px-10 py-3 rounded-full hover:bg-gray-700 duration-500"
