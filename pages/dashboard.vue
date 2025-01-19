4b14c653e56c7a60603a1328eb41cb3315797ae4
Cyrus Yip <cyrusyip@cyrusyip.com>
2025-01-18 21:40:04 -0500

    dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>
-    <h1>Users</h1>
+  <div class="flex items-center justify-center flex-col gap-5 p-10 w-full">
+    <div
+      v-for="user in users"
+      class="w-full max-w-xl bg-gray-800/50 p-5 rounded-lg shadow-lg flex-col items-center gap-5"
+    >
+      <h2 class="text-lg">{{ user.name }}</h2>
+      <p class="text-sm">
+        <span :class="{ underline: user.year == data.year }"
+          >U{{ user.year }}</span
+        >
+        {{ user.major }}
+      </p>
+      <div class="flex gap-5">
+        <p v-for="course in user.courses" class="text-sm">{{ course }}</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import courses from "~/courses.txt?raw";
+
+const courses_list = courses.split("\n");
+function formattedCourses(list) {
+  let res = [];
+  for (const course of list) {
+    res.push(course.replace(/([a-zA-Z]+)(\d+)/, "$1 $2"));
+  }
+  return res;
+}
+const refactored_courses = formattedCourses(courses_list);
+const users = [];
+for (let i = 0; i < 10; i++) {
+  let courses = new Set();
+  let number_of_courses = Math.floor(Math.random() * 3) + 3;
+  for (let j = 0; j < number_of_courses; j++) {
+    courses.add(
+      refactored_courses[Math.floor(Math.random() * refactored_courses.length)]
+    );
+  }
+  users.push({
+    name: "John Doe",
+    major: "Computer Science",
+    year: Math.floor(Math.random() * 5),
+    courses: courses,
+  });
+}
+
+const data = ref({});
+
+onMounted(() => {
+  data.value = localStorage.getItem("user");
+  if (!data.value) {
+    navigateTo("/");
+  } else {
+    data.value = JSON.parse(data.value);
+  }
+});
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,40 +1,28 @@
 <template>
   <form
-      class="flex items-stretch justify-center flex-col gap-7 w-full min-h-screen max-w-2xl px-7"
-      @submit.prevent
+    class="flex items-stretch justify-center flex-col gap-7 w-full min-h-screen max-w-2xl px-7"
+    @submit.prevent
   >
     <h1 class="text-xl text-center">Log Into Your Account</h1>
     <div class="flex flex-col gap-2">
       <label for="name">Name</label>
       <input
-          type="text"
-          id="name"
-          class="border-2 border-white rounded px-5 py-2 bg-inherit outline-none placeholder:text-white/80"
-          placeholder="John Doe"
+        type="text"
+        id="name"
+        class="border-2 border-white rounded px-5 py-2 bg-inherit outline-none placeholder:text-white/80"
+        placeholder="John Doe"
       />
     </div>
     <div class="flex flex-col gap-2">
       <label for="email">Email</label>
       <input
-          type="email"
-          id="email"
-          class="border-2 border-white rounded px-5 py-2 bg-inherit outline-none placeholder:text-white/80"
-          placeholder="student@mail.mcgill.ca"
+        type="email"
+        id="email"
+        class="border-2 border-white rounded px-5 py-2 bg-inherit outline-none placeholder:text-white/80"
+        placeholder="student@mail.mcgill.ca"
       />
     </div>
   </form>
 </template>
 
-<script setup>
-import courses from "~/courses.txt?raw";
-
-const courses_list = courses.split("\n");
-const course = ref("");
-
-const user_courses = reactive(new Set());
-const autocomplete = computed(() => {
-  return courses_list
-      .filter((c) => c.toLowerCase().startsWith(course.value.toLowerCase()))
-      .slice(0, 1000);
-});
-</script>
+<script setup></script>
